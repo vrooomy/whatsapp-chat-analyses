be426bdacf756d15dab552cfdc28a086dae03f89
vrooomy <vj.bro.833@gmail.com>
2025-02-08 21:16:12 +0530

    initial commmit

---
+++ app.py
@@ -0,0 +1,124 @@
+import streamlit as st
+import preprocessor,helper
+import matplotlib.pyplot as plt
+
+st.sidebar.title("Whatsapp chat Analyser")
+
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    # bytes_data = uploaded_file.getvalue()
+    # data = bytes_data.decode("utf-8")
+    #st.text(data)
+
+    data = uploaded_file.read().decode("utf-8").splitlines()
+
+    df = preprocessor.preprocess(data)
+
+    df.drop(columns=['user_message'] , inplace =True) 
+
+
+    # st.dataframe(df)
+
+
+    #fetch unique users
+
+    user_list= df['user'].unique().tolist()
+    user_list.remove('group_notif')
+    user_list.sort()
+    user_list.insert(0,"Overall")
+
+    selected_user = st.sidebar.selectbox("Show analysis wrt",user_list)
+
+
+    if st.sidebar.button("Show Analysis"):
+
+        #stats area
+        num_messages,words,num_media_msg, num_links = helper.fetch_stats(selected_user,df)
+
+        st.title("Top Statistics")
+        
+
+        col1, col2 , col3, col4 = st.columns(4)
+
+        with col1:
+            st.header("All Messages")
+            st.title(num_messages)
+
+        with col2:
+            st.header("Total words")
+            st.title(words)
+
+        with col3:
+            st.header("Media shared")
+            st.title(num_media_msg)
+
+        with col4:
+            st.header("Links Shared")
+            st.title(num_links)
+
+        
+        #monthly_timeline
+        st.title('Monthly Activity')
+        timeline = helper.monthly_timeline(selected_user,df)
+
+        fig,ax = plt.subplots()
+        ax.plot(timeline['time'],timeline['message'],color = 'green')
+        plt.xticks(rotation='vertical')
+
+        st.pyplot(fig)
+
+
+        #daily_timeline
+        st.title('Daily activity')
+        daily_timeline = helper.daily_timeline(selected_user,df)
+
+        fig,ax = plt.subplots()
+        ax.plot(daily_timeline['only_date'],daily_timeline['message'],color = 'black')
+        plt.xticks(rotation='vertical')
+
+        st.pyplot(fig)
+
+
+
+        #most active user in group
+        if selected_user == 'Overall':
+            st.title('Most Active Users')
+            x, new_df = helper.most_busy_users(df)
+            fig, ax = plt.subplots()
+           
+
+            col1 , col2 = st.columns(2)
+
+            with col1:
+                ax.bar(x.index,x.values,color = 'red')
+                plt.xticks(rotation = 'vertical')
+                st.pyplot(fig)
+            
+            with col2:
+                st.dataframe(new_df)
+
+        #wordcloud
+        st.title("Word Cloud")
+        df_wc = helper.create_wordcloud(selected_user,df)
+        fig,ax= plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+
+
+        #most comon words
+        st.title("Word Frequency")
+        most_common_df = helper.most_common_words(selected_user, df)
+
+        fig, ax = plt.subplots()
+
+        ax.barh(most_common_df[0],most_common_df[1])
+
+        st.pyplot(fig)
+
+        st.dataframe(most_common_df)
+
+
+
+    
---
+++ preprocessor.py
@@ -0,0 +1,64 @@
+import re
+import pandas as pd
+
+
+# file_path='C:\Users\vjbro\OneDrive\Desktop\STUDY\projects\analytics\WhatsApp Chat with Oncampus placement 2025\WhatsApp Chat with Oncampus placement 2025 .txt'
+# with open(file_path, 'r', encoding='utf-8') as file:
+#     data = file.readlines()
+
+# print(preprocess(data))
+
+
+
+def preprocess(data):
+    pattern = r'^(\d{2}/\d{2}/\d{2,4}, \d{1,2}:\d{2}\s?[ap]m) - (.+)$'
+
+
+    dates, messages_only = [], []
+
+    for msg in data:
+        match = re.match(pattern, msg.strip())
+        if match:
+            dates.append(match.group(1))          # Extracted date
+            messages_only.append(match.group(2)) # Extracted message
+
+    df = pd.DataFrame({'user_message': messages_only, 'message_date': dates})
+
+    # Convert 'message_date' to datetime format
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %I:%M %p')
+
+    # Rename 'message_date' to 'date' (optional)
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    message_pattern = '([^:]+): (.+)'
+
+    users = []
+    message = []
+
+    for m in df['user_message']:
+        entry = re.split(message_pattern, m)
+        if entry[1:]:
+            users.append(entry[1])
+            message.append(entry[2])
+        else:
+            users.append('group_notif')
+            message.append(m)
+
+    df['user']= users
+    df['message'] = message
+
+
+    df['only_date']= df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num']= df['date'].dt.month
+    df['month']=df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+
+    df['hour']= df['date'].dt.hour
+    df['minute']= df['date'].dt.minute
+
+
+    return df
+
+
+
